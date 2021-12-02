73fcbc9b622fa7806f4397f04d5b4cf0f38a2e12
Paul Hanzlik <pjhanzlik@tutanota.com>
2021-12-02 15:59:53 -0600

    unstyled, unsharable, quote machine

--- random quote machine/RandomQuoteMachine.js
+++ random quote machine/RandomQuoteMachine.js
@@ -1,39 +1,46 @@
+const INITIAL_TEMPLATE = document.createElement("template");
+INITIAL_TEMPLATE.innerHTML = `<output for="next">
+    <figure id="description">
+        <blockquote id="quote"></blockquote>
+        <figcaption id="caption"></figcaption>
+    </figure>
+</output>
+<a href="." id="email" aria-describedby="description">Email</a>
+<button id="next" type="button" disabled>New quote</button>`;
+
 export default class extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({mode: "open"});
-        this.shadowRoot.append(this.constructor.initialTemplate.content.cloneNode(true));
-        this.aborter = new AbortController();
+        this.shadowRoot.append(INITIAL_TEMPLATE.content.cloneNode(true));
+        this.next = this.shadowRoot.getElementById("next");
+        this.next.onclick = this.newQuote;
     }
 
-    static observedAttributes {
-        return ["data-quotes"];
+    static get observedAttributes() { return ["data-quotes"] };
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        this.handleQuotesChange(newValue);
     }
 
-    attributeChangeCallback(name, oldValue, newValue) {
-        // clear parallel work done by previous data-quotes change
-        this.aborter.abort();
-        async () => {
-            this.response = await fetch(newValue, {signal: this.aborter.signal});
-    //      this.quotes = await read response as json
+    handleQuotesChange = async (url) => {
+        const response = await fetch(url);
+        this.quotes = await response.json();
 
-            // TODO verify proper json structure?
-            
-            // enable or disable new quote button based on fetched json
-            if(this.quotes.length <= 1) {
-                this.shadowRoot.getElementById("new").disabled = true;
+        // enable or disable new quote button based on fetched json
+        if(this.quotes.length <= 1) {
+            this.shadowRoot.getElementById("new").disabled = true;
+        }
+        else {
+            if(this.next.disabled) {
+                this.next.disabled = false;
             }
-            else {
-                if(this.shadowRoot.getElementById("new").disabled) {
-                    this.shadowRoot.getElementById("new").disabled = false;
-                }
-            }
+        }
 
-            this.quoteIndex = undefined;
+        this.quoteIndex = undefined;
 
-            if(this.quotes.length >= 1) {
-                this.newQuote();
-            }
+        if(this.quotes.length >= 1) {
+            this.newQuote();
         }
     }
 
@@ -41,21 +48,8 @@ export default class extends HTMLElement {
         const newQuotes = this.quoteIndex ? 
             Array.join(this.quotes[0,this.quoteIndex],this.quotes[this.quoteIndex]) :
             this.quotes;
-        const randomIndex = Math.floor(Math.random() * this.newQuotes.length);
-        this.shadowRoot.getElementById("body").textContent = newQuotes[randomIndex].body;
-        this.shadowRoot.getElementById("subject") = newQuotes[randomIndex].subject;
-    }
-
-    static {
-        this.initialTemplate = document.createElement("template");
-        this.initialTemplate.innerHTML = `<output for="new">
-            <figure>
-            <blockquote id="body"></blockquote>
-            <figcaption id="subject"></figcaption>
-            </figure>
-        </output>
-        <a id="email">Email</a>
-        <button id="new" type="button" disabled>New quote</button>
-        `
+        const randomIndex = Math.floor(Math.random() * newQuotes.length);
+        this.shadowRoot.getElementById("caption").textContent = newQuotes[randomIndex].caption;
+        this.shadowRoot.getElementById("quote").textContent = newQuotes[randomIndex].quote;
     }
 }
