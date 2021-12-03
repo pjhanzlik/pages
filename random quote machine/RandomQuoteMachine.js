a54f7f0b9131f0d7fb0168935b9ff1b8a9a2bd46
Paul Hanzlik <pjhanzlik@tutanota.com>
2021-12-03 16:47:04 -0600

    Rough draft for quote machine

--- random quote machine/RandomQuoteMachine.js
+++ random quote machine/RandomQuoteMachine.js
@@ -1,55 +1,132 @@
 const INITIAL_TEMPLATE = document.createElement("template");
-INITIAL_TEMPLATE.innerHTML = `<output for="next">
+INITIAL_TEMPLATE.innerHTML = `
+<style>
+:host,figure {
+    display: grid;
+}
+:host {
+    border-radius: 1em;
+    --glyph-color: white;
+    --space-color: black;
+    padding: 1em;
+    grid-template-areas: "output output" "share next";
+    background-color: var(--space-color);
+    color: var(--glyph-color);
+    font-family: sans-serif;
+    text-align: center;
+    gap: 1em;
+}
+figure {
+    gap: 0.3em;
+    font-size: 1.25em;
+}
+:disabled {
+    opacity: 0;
+}
+:any-link {
+    text-decoration: unset;
+}
+button {
+    background-color: unset;
+    font-size: unset;
+    text-align: unset;
+    padding: unset;
+    border: unset;
+    font-family: unset;
+}
+button, :any-link {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: var(--space-color);
+    background-color: var(--glyph-color);
+    border-radius: 4px;
+    border: solid 4px var(--glyph-color);
+}
+output {
+    grid-area: output; 
+}
+blockquote,figure {
+    margin: 0;
+}
+#share {
+    grid-area: share;
+}
+#next {
+    grid-area: next;
+}
+blockquote {
+    font-weight: bold;
+    font-size: 1.25em;
+}
+</style>
+<output for="next">
     <figure id="description">
         <blockquote id="quote"></blockquote>
         <figcaption id="caption"></figcaption>
     </figure>
 </output>
-<a href="." id="email" aria-describedby="description">Email</a>
-<button id="next" type="button" disabled>New quote</button>`;
+<a id="share" part="button" rel="noopener" target="_blank" aria-describedby="description">Email</a>
+<button id="next" part="button" type="button" disabled>New quote</button>`;
 
 export default class extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({mode: "open"});
+
         this.shadowRoot.append(INITIAL_TEMPLATE.content.cloneNode(true));
-        this.next = this.shadowRoot.getElementById("next");
-        this.next.onclick = this.newQuote;
+        this._caption = this.shadowRoot.getElementById("caption");
+        this._share = this.shadowRoot.getElementById("share");
+        this._quote = this.shadowRoot.getElementById("quote");
+        this._next = this.shadowRoot.getElementById("next");
+        this._next.onclick = () => {
+            this.nextQuote();
+            this.scrollIntoView(true);
+        }
     }
 
     static get observedAttributes() { return ["data-quotes"] };
 
-    attributeChangedCallback(name, oldValue, newValue) {
-        this.handleQuotesChange(newValue);
+    attributeChangedCallback(name, oldValue, nextValue) {
+        this.fetchQuotes(nextValue);
     }
 
-    handleQuotesChange = async (url) => {
-        const response = await fetch(url);
-        this.quotes = await response.json();
+    fetchQuotes = async (url) => {
+        if(this._prevFetch) {
+            this._prevFetch.abort();
+        }
+        this._prevFetch = new AbortController();
+        const response = await fetch(url, {signal: this._prevFetch.signal});
+        this.unread = await response.json();
+        this.nextQuote();
+    }
 
-        // enable or disable new quote button based on fetched json
-        if(this.quotes.length <= 1) {
-            this.shadowRoot.getElementById("new").disabled = true;
+    nextQuote = () => {
+        const next = Math.floor(Math.random()*this.unread.length);
+        this._quote.textContent = this.unread[next].quote;
+        this._caption.textContent = this.unread[next].caption;
+        this._share.href = `mailto:?subject=${this.unread[next].quote}&body=${this.unread[next].caption}`;
+        if(this.unread[next].source) {
+            this._quote.cite = this.unread[next].source;
         }
         else {
-            if(this.next.disabled) {
-                this.next.disabled = false;
-            }
+            this._quote.removeAttribute("cite");
         }
 
-        this.quoteIndex = undefined;
-
-        if(this.quotes.length >= 1) {
-            this.newQuote();
-        }
+        this.unread = this.unread.slice(0,next).concat(this.unread.slice(next+1));
     }
 
-    newQuote = () => {
-        const newQuotes = this.quoteIndex ? 
-            Array.join(this.quotes[0,this.quoteIndex],this.quotes[this.quoteIndex]) :
-            this.quotes;
-        const randomIndex = Math.floor(Math.random() * newQuotes.length);
-        this.shadowRoot.getElementById("caption").textContent = newQuotes[randomIndex].caption;
-        this.shadowRoot.getElementById("quote").textContent = newQuotes[randomIndex].quote;
+    set unread(array) {
+        if(array.length) {
+            this._next.disabled = false;
+        }
+        else {
+            this._next.disabled = true;
+        }
+        this._unread = array;
+    }
+
+    get unread() {
+        return this._unread;
     }
 }
