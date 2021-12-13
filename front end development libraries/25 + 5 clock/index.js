a48e7132127d528fbad45079d621364c758c15d9
Paul Hanzlik <pjhanzlik@tutanota.com>
2021-12-13 15:29:37 -0600

    Oops, accidentally commited quote prototype stash

--- front end development libraries/25 + 5 clock/index.js
+++
@@ -1,8 +0,0 @@
-import Reducers from './reducers.js';
-import App from './App.js';
-
-const STORE = Redux.createStore(Reducers);
-const APP = React.createElement(App);
-const PROVIDER = React.createElement(ReactRedux.Provider, { store: STORE }, APP);
-
-ReactDOM.render(PROVIDER, document.getElementById('root'));
