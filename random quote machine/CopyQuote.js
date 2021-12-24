70689ffd0e1f3e0f2d512c625cfc4993a8fb6e3e
Paul Hanzlik <pjhanzlik@tutanota.com>
2021-12-24 15:56:00 -0600

    Quick copy button prototype before xmas

--- random quote machine/CopyQuote.js
+++ random quote machine/CopyQuote.js
@@ -3,13 +3,43 @@ INIT.innerHTML = `<style>
 [part=button]:disabled {
     opacity: 0;
 }
+[part=button]:active:hover::after {
+    content: "ing";
+}
 </style>
-<button part="button" type="button" disabled>Copy</button>`
+<button id="the-button" part="button" type="button" disabled>Copy</button>`
 
 export default class extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({mode: "open"});
         this.shadowRoot.append(INIT.content.cloneNode(true));
+        const theButton = this.shadowRoot.getElementById("the-button");
+        theButton.onclick = async () => {
+            const quote = document.getElementById(this.dataset.quote);
+            try {
+                await navigator.clipboard.writeText(`${quote.textContent} ${quote.cite}`);
+            }
+            catch(e) {
+                console.debug(e);
+            }
+        }
+    }
+
+    static get observedAttributes() {
+        return ["data-quote"];
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        const theButton = this.shadowRoot.getElementById("the-button");
+        const newTarget = document.getElementById(newValue);
+        if(newTarget instanceof HTMLQuoteElement) {
+            theButton.disabled = false;
+            theButton.setAttribute("aria-details", newValue);
+        }
+        else {
+            theButton.disabled = true;
+            theButton.removeAttribute("aria-details");
+        }
     }
 }
